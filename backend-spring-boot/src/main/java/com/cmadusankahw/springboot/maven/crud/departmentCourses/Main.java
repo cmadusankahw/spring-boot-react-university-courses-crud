7f3958f8a3b5dfac33048520866ad7d4c933304f
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-10-17 16:16:14 +0530

    project initial commit

---
+++ backend-spring-boot/src/main/java/com/cmadusankahw/springboot/maven/crud/departmentCourses/Main.java
@@ -0,0 +1,13 @@
+package com.cmadusankahw.springboot.maven.crud.departmentCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
